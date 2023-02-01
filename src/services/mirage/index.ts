1ec9e81916e3597100d5975d8e863d314dcfe367
Ariel Marte <ariel.amas@hotmail.com>
2023-02-01 17:09:38 -0300

    Inicio MirageJS

---
+++ src/services/mirage/index.ts
@@ -0,0 +1,23 @@
+import { createServer, Model } from 'miragejs'
+
+type User = {
+    name: string;
+    email: string;
+    created_at: string;
+}
+
+export function makeServer(){
+    const server = createServer({
+        models:{
+            user: Model.extend<Partial<User>>({})
+
+        },
+
+        routes(){
+            this.get('/users');
+        }
+    })
+
+
+}
+
